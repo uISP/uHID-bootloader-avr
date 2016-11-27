fa2975ed8dd3b7c7434d7d610c8666f084d3c5d0
Andrew Andrianov <andrew@ncrmnt.org>
2016-11-27 21:07:58 +0300

    uhid: Rework bootloader to work under windows
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/uisp-loader.c
+++ src/uisp-loader.c
@@ -8,6 +8,8 @@
 #include <arch/vusb/usbdrv.h>
 #include "uisp.h"
 
+
+
 static void (*nullVector)(void) __attribute__((__noreturn__));
 
 #ifndef GICR
@@ -41,6 +43,7 @@ struct partInfo {
 }  __attribute__((packed));
 
 struct deviceInfo {
+	uint8_t       version;
 	uint8_t       numParts;
 	uint8_t       cpuFreq;
 	struct partInfo parts[];
@@ -56,6 +59,7 @@ struct deviceInfo {
 #endif
 
 static const PROGMEM struct deviceInfo devInfo = {
+	.version = 1,
 	.cpuFreq = F_CPU / 100000,
 
 #ifdef CONFIG_UHID_EEPROM
@@ -65,7 +69,7 @@ static const PROGMEM struct deviceInfo devInfo = {
 #endif
 
 	.parts = {
-		{SPM_PAGESIZE, CONFIG_AVR_BLDADDR, 6,       "flash"  },
+		{SPM_PAGESIZE, CONFIG_AVR_BLDADDR, IOBUFLEN,       "flash"  },
 #ifdef CONFIG_UHID_EEPROM
 		{SPM_PAGESIZE, E2END + 1,          IOBUFLEN,       EEP_PART_NAME }
 #endif
@@ -81,18 +85,18 @@ const PROGMEM char usbHidReportDescriptor[42] = {
 	0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
 	0x75, 0x08,                    //   REPORT_SIZE (8)
 
-	0x85, 0x00,                    //   REPORT_ID (0)
-	0x95, 0xff,                    //   REPORT_COUNT (255)
-	0x09, 0x00,                    //   USAGE (Undefined)
-	0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
-
 	0x85, 0x01,                    //   REPORT_ID (1)
-	0x95, IOBUFLEN + 1,                    //   REPORT_COUNT (6)
+	0x95, IOBUFLEN,                    //   REPORT_COUNT (255)
 	0x09, 0x00,                    //   USAGE (Undefined)
 	0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
 
 	0x85, 0x02,                    //   REPORT_ID (2)
-	0x95, IOBUFLEN + 1,                    //   REPORT_COUNT (131)
+	0x95, IOBUFLEN,                    //   REPORT_COUNT (6)
+	0x09, 0x00,                    //   USAGE (Undefined)
+	0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
+
+	0x85, 0x03,                    //   REPORT_ID (3)
+	0x95, IOBUFLEN,                    //   REPORT_COUNT (131)
 	0x09, 0x00,                    //   USAGE (Undefined)
 	0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
 
@@ -112,79 +116,95 @@ typedef uint16_t addr_t;
 #endif
 
 static addr_t addr;
-
+static char opStart;
 
 uchar   usbFunctionSetup(uchar data[8])
 {
 	usbRequest_t    *rq = (void *)data;
+	opStart = 1;
+
 	/* Avoid pointer some pointer derefs, they eat up flash */
 	target = rq->wValue.bytes[0];
 	wLength = rq->wLength.bytes[0];
-	uint8_t wValue = rq->wValue.bytes[0];
 	uint8_t bRequest = rq->bRequest;
 	if (bRequest == USBRQ_HID_SET_REPORT) {
-		if (!wValue)
+		if (!target)
 			leaveBootloader();
-		else
-			return USB_NO_MSG;
+		return USB_NO_MSG;
 	} else if(bRequest == USBRQ_HID_GET_REPORT) {
+		replyBuffer[0]=target;
 		usbMsgPtr = replyBuffer;
-		if (wValue==0) {
-			do_memcpy(replyBuffer, &devInfo, UISP_INFO_LEN);
+		if (target==1) {
 			addr = 0;
-			return UISP_INFO_LEN;
-		} else if (wValue == 1) {
-			do_memcpy(replyBuffer, (void *) addr, wLength);
-		} else {
-
-#ifdef CONFIG_UHID_EEPROM_READBACK
-			eeprom_read_block(replyBuffer, (void *) addr, wLength);
-#endif
+			do_memcpy(&replyBuffer[1], &devInfo, UISP_INFO_LEN);
+			return UISP_INFO_LEN+1;
+		} else if (target == 2) {
+			do_memcpy(&replyBuffer[1], (void *) addr, IOBUFLEN);
 		}
-		addr += wLength;
-		return wLength;
+#ifdef CONFIG_UHID_EEPROM_READBACK
+		else {
+			eeprom_read_block(&replyBuffer[1], (void *) addr, wLength);
+		}
+#endif
+		addr += IOBUFLEN;
+		return IOBUFLEN+1;
 	}
 	return 0;
+}
+
+static void flash_write_word(uint16_t data)
+{
+	cli();
+	boot_page_fill(addr, data);
+	sei();
+
+	addr+=2;
+
+	if ((addr & (SPM_PAGESIZE - 1)) == 0) {
+		addr_t prev = addr - 2;
+		boot_page_erase(prev); /* erase page */
+		boot_spm_busy_wait();
+		cli();
+		boot_page_write(prev);
+		sei();
+		boot_rww_enable_safe();
+	}
+}
+
+static char flash_byte_pos;
+usbWord_t flashTmp;
+static void flash_write_byte(uchar byte)
+{
+	int pos = flash_byte_pos & 0x1;
+	flashTmp.bytes[pos] = byte;
+	if (pos)
+		flash_write_word(flashTmp.word);
+
+	flash_byte_pos++;
 }
 
 uchar usbFunctionWrite(uchar *data, uchar len)
 {
 	wLength -= len;
-
 	/* Discard reportId */
-	data++;
-	len--;
+	if (opStart) {
+		data++;
+		len--;
+		opStart=0;
+	}
 
-	if (target == 2) {
+	if (target == 3) {
 #ifdef CONFIG_UHID_EEPROM
 		eeprom_write_block(data, (void *) addr, len);
 		addr += len;
 #endif
 	} else {
-
-		do {
-
-			cli();
-			boot_page_fill(addr, *((uint16_t *) data));
-			sei();
-
-			data+=2;
-			addr+=2;
-			len-=2;
-
-			if ((addr & (SPM_PAGESIZE - 1)) == 0) {
-				addr_t prev = addr - 2;
-				boot_page_erase(prev); /* erase page */
-				boot_spm_busy_wait();
-				cli();
-				boot_page_write(prev);
-				sei();
-				boot_rww_enable_safe();
-			}
-		} while (len);
+		 while (len--) {
+			flash_write_byte(*data++);
+		};
 	}
-
-	return (wLength == 0);
+	/* wLength < 1 takes 10 bytes less that wLength == 0 */
+    return (wLength < 1);
 }
 
 
@@ -198,7 +218,7 @@ int main()
 
 	DDRD |= (1<<CONFIG_USB_CFG_DMINUS_BIT) | (1<<CONFIG_USB_CFG_DMINUS_BIT);
 	PORTD &= ~((1<<CONFIG_USB_CFG_DMINUS_BIT) | (1<<CONFIG_USB_CFG_DMINUS_BIT));
-	_delay_ms(25);
+	_delay_ms(5);
 	DDRD &= ~((1<<CONFIG_USB_CFG_DMINUS_BIT) | (1<<CONFIG_USB_CFG_DMINUS_BIT));
 
 	sei();
@@ -216,3 +236,5 @@ int main()
 #endif
 
 }
+
+#include "../antares/src/lib/contrib/vusb/usbdrv/usbdrv.c"
