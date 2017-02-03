0093456d186b6992c5eeebeb9228ccb08980c5c3
Andrew Andrianov <andrew@ncrmnt.org>
2017-02-03 14:37:38 +0300

    main-lufa: avoid delay in linux due to hidraw driver
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- src/main-lufa.c
+++ src/main-lufa.c
@@ -1,28 +1,28 @@
- #include <avr/io.h>
- #include <avr/wdt.h>
- #include <avr/power.h>
- #include <avr/interrupt.h>
- #include <stdbool.h>
- #include <string.h>
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include <string.h>
 #include <uhid.h>
- #include <LUFA/Drivers/USB/USB.h>
+#include <LUFA/Drivers/USB/USB.h>
 
 
- #ifndef GICR
- #define GICR    MCUCR
- #endif
+#ifndef GICR
+#define GICR    MCUCR
+#endif
 
- void reconnect()
- {
- 	USB_Detach();
- 	_delay_ms(50);
- 	USB_Attach();
- }
+void reconnect()
+{
+	USB_Detach();
+	_delay_ms(50);
+	USB_Attach();
+}
 
 int main(void)
 {
-	GICR = (1 << IVCE) ;  /* enable change of interrupt vectors */
-	GICR = (1 << IVSEL); /* move interrupts to boot flash section */
+	GICR = (1 << IVCE);     /* enable change of interrupt vectors */
+	GICR = (1 << IVSEL);    /* move interrupts to boot flash section */
 
 	initRunButton();
 	USB_Init();
@@ -37,34 +37,35 @@ int main(void)
 
 	while (1) {
 		USB_USBTask();
-#ifdef	CONFIG_RUN_BUTTON_ON_LOOP
+#ifdef  CONFIG_RUN_BUTTON_ON_LOOP
 		if (checkRunButton())
 			leaveBootloader();
 #endif
+
 	}
 }
 
-
 void EVENT_USB_Device_ConfigurationChanged(void)
 {
-   Endpoint_ConfigureEndpoint(UHID_IN_EPADDR, EP_TYPE_INTERRUPT, UHID_IN_EPSIZE, 1);
+	Endpoint_ConfigureEndpoint(UHID_IN_EPADDR, EP_TYPE_INTERRUPT, UHID_IN_EPSIZE, 1);
 }
 
 void *usbMsgPtr;
 #include "bootlogic.c"
 void EVENT_USB_Device_ControlRequest(void)
 {
-
 	uint8_t type = USB_ControlRequest.bmRequestType;
+
 	type &= (REQTYPE_CLASS | REQREC_INTERFACE);
 
-	if (type == (REQTYPE_CLASS | REQREC_INTERFACE))
-	{
+	if (type == (REQTYPE_CLASS | REQREC_INTERFACE)) {
 		uint8_t ret = usbFunctionSetup((void *)&USB_ControlRequest);
+		if (!ret)
+			return;
 		Endpoint_ClearSETUP();
 		if (ret == 0xff) { /* USB_NO_MSG */
-			Endpoint_Read_Control_Stream_LE(replyBuffer, IOBUFLEN+1);
-			usbFunctionWrite(replyBuffer, IOBUFLEN+1);
+			Endpoint_Read_Control_Stream_LE(replyBuffer, IOBUFLEN + 1);
+			usbFunctionWrite(replyBuffer, IOBUFLEN + 1);
 			Endpoint_ClearIN();
 		} else if (ret > 0) {
 			Endpoint_Write_Control_Stream_LE(usbMsgPtr, ret);
